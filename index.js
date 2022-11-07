241c7069de3f63dd4f57d571f981af66ac65c5b3
Taslim <taslimahmed313@gmil.com>
2022-11-08 02:03:23 +0600

    Get Services With Uniqe Id

--- index.js
+++ index.js
@@ -1,11 +1,62 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4nt1ond.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run(){
+    try{
+        const serviceCollection = client.db("userReview").collection('services');
+
+        app.get('/service', async(req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(3).toArray();
+            res.send(services);
+        })
+
+        app.get('/services', async(req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+        app.get('/services/:id', async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await serviceCollection.findOne(query);
+            res.send(result);
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(e => console.error(e));
+
+
+
+
+
+
+
 
 app.get('/', (req, res)=>{
     res.send('Personal Portfolio Data Are Coming..........!!!')
